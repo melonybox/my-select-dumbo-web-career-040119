fc99e2d85b2cd75349e3ea24bf2bf897cb1f5d60
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 06:55:50 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(list)
+  i = 0
+  newList = []
+  
+  while i < list.size
+    if yield(list[i])
+      newList.push(list[i])
+    end
+    i = i + 1
+  end
+  return newList
 end
